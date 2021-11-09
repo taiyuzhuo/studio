e991fc98066fb8ae6d61261b536b59d5d9442dcf
Andrew Carr <andrew@foxglove.dev>
2021-11-10 08:09:42 +1100

    Just memo against theme as it doesn't change that often

--- packages/studio-base/src/components/TopicToRenderMenu.tsx
+++ packages/studio-base/src/components/TopicToRenderMenu.tsx
@@ -114,14 +114,7 @@ export default function TopicToRenderMenu({
         },
       },
     }),
-    [
-      availableTopics,
-      onChange,
-      renderTopics,
-      theme.fonts.small.fontSize,
-      theme.palette.themePrimary,
-      topicToRender,
-    ],
+    [availableTopics, onChange, renderTopics, theme, topicToRender],
   );
 
   return (
